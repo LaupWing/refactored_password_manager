38d38bcc5981e7da8c415cc98ab15471de593c25
LaupWing <loc_strict@hotmail.com>
2021-06-17 17:13:17 +0200

    default tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
    darkMode: false, // or 'media' or 'class'
    theme: {
-      extend: {},
+      extend: {
+         colors:{
+            black:{
+               DEFAULT: 'black',
+               default:'#1E1E1E',
+               light: '#282828',
+               lightest: '#404040'
+            },
+            mainBlue: '#325DA8',
+            mainGrey: '#9E9E9E'
+         },
+      },
    },
    variants: {
       extend: {},
